0658819123f528d8fb86c6d4b6199dd441038ec8
wtsclwq <lwqcode@foxmail.com>
2022-08-01 23:03:19 +0800

    pointers and errors passed

---
+++ pointers/wallet.go
@@ -0,0 +1,35 @@
+package pointers
+
+import (
+	"errors"
+	"fmt"
+)
+
+type Bitcoin int
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
+
+var ErrfooInsufficientFunds = errors.New("cannot withdraw, insufficient funds")
+
+type Wallet struct {
+	balance Bitcoin
+}
+
+func (w *Wallet) Deposit(amount Bitcoin) {
+	w.balance += amount
+}
+
+func (w *Wallet) Balance() Bitcoin {
+	return w.balance
+}
+
+func (w *Wallet) Withdraw(amount Bitcoin) error {
+
+	if amount > w.balance {
+		return ErrfooInsufficientFunds
+	}
+	w.balance -= amount
+	return nil
+}
