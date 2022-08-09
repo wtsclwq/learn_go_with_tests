543bf170579ae3652c700cb4cbeff392146a5e82
wtsclwq <lwqcode@foxmail.com>
2022-08-09 21:15:21 +0800

    reflection passed

--- hello/hello.go
+++ hello/hello.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 const englishHelloPrefix = "Hello, "
 const FrenchHelloPrefix = "Bonjour, "
 const SpanishHelloPrefix = "Hola, "
@@ -25,5 +27,23 @@ func greetingPrefix(language string) (prefix string) {
 	}
 	return
 }
+
 func main() {
+	a := F()
+	a[0]() //0xc00004c080 0
+	a[1]() //0xc00004c088 1
+	a[2]() //0xc00004c090 2
+	a[0]()
+}
+func F() []func() {
+	b := make([]func(), 3)
+	for i := 0; i < 3; i++ {
+		j := i
+		b[i] = func() {
+			fmt.Println(&j, j)
+			j++
+			fmt.Println(&j, j)
+		}
+	}
+	return b
 }
